52cd8a2b113e6391238c01c54c07ee32fca01944
Paul Samuels <p.samuels@mmu.ac.uk>
2013-02-03 18:01:40 +0000

    Cleaned up UI and added queries override

--- lib/sbconstants/options.rb
+++ lib/sbconstants/options.rb
@@ -3,27 +3,31 @@ require 'yaml'
 
 module SBConstants
   class Options
-    attr_accessor :dry_run, :prefix, :destination, :query_config, :output_path, :source_dir, :debug
+    attr_accessor :dry_run, :prefix, :destination, :query_config, :output_path, :source_dir, :verbose, :query_file
     
     def self.parse argv
       options = self.new
       OptionParser.new do |opts|
         opts.banner = "Usage: DESTINATION_FILE [options]"
-  
-        opts.on('--dry-run') do |dry_run|
-          options.dry_run = dry_run
-        end
-  
-        opts.on('-p', '--prefix PREFIX') do |prefix|
+    
+        opts.on('-p', '--prefix=<prefix>', 'Only match identifiers with <prefix>') do |prefix|
           options.prefix = prefix
         end
         
-        opts.on('-s', '--source-dir SOURCE') do |source_dir|
+        opts.on('-s', '--source-dir=<source>', 'Directory containing storyboards') do |source_dir|
           options.source_dir = source_dir
         end
         
-        opts.on('-d') do |debug|
-          options.debug = debug
+        opts.on('-q', '--queries=<queries>', 'YAML file containing queries') do |queries|
+          options.query_file = queries
+        end
+        
+        opts.on('-d', '--dry-run', 'Output to STDOUT') do |dry_run|
+          options.dry_run = dry_run
+        end
+        
+        opts.on('-v', '--verbose', 'Verbose output') do |verbose|
+          options.verbose = verbose
         end
       end.parse!(argv)
       
@@ -36,9 +40,9 @@ module SBConstants
       options
     end
     
-    def initialize query_file=nil
-      @query_file = query_file || File.expand_path('../../sbconstants/identifiers.yml', __FILE__)
-      @source_dir = Dir.pwd
+    def initialize
+      self.query_file = File.expand_path('../../sbconstants/identifiers.yml', __FILE__)
+      self.source_dir = Dir.pwd
     end
     
     def queries
@@ -50,14 +54,14 @@ module SBConstants
     
     def load_queries
       @queries = []
-      config = YAML.load(File.open(@query_file).read)
+      config = YAML.load(File.open(query_file).read)
       config.each do |nodes, identifiers|
         Array(nodes).each do |node|
           Array(identifiers).each do |identifier|
             @queries << Query.new(node, identifier)
           end
         end
-      end      
+      end
     end
   end
 end
